b4dd0a8e54c2d49418783ab6fac68223ca6211c7
David Willis <david@ravenflight.media>
2021-12-23 11:33:50 -0600

    fixed chart data, added rollup

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
-let updateInterval = 5, //in minutes
-	lastFullUpdate = lastPartialUpdate = "none",
-	i = 60 / updateInterval; //update forecast once an hour
-			
+/*	Weather Station
+ *	david@ravenflight.media
+*/	
+
+import Chart from 'chart.js/auto';
+
+let apikey = "48ce79e682e5e8f79e39cc1374871d75",
+	url_current = "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=36.16754647878633&lon=-86.21153419024921&appid="+apikey,
+	url_forecast = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=36.16754647878633&lon=-86.21153419024921&appid="+apikey,
+	//url_map = "https://tile.openweathermap.org/map/precipitation_new/7/36.1467/-86.8250.png?appid="+apikey,
+	updateInterval = 10, //in minutes
+	fullupdateFrequency = 60 / updateInterval; //update forecast once an hour
+	
 document.querySelector("body").addEventListener("click", function(){
 	document.documentElement.requestFullscreen();
 });
-			
+
 const readableTime = function(str = Date.now(), seconds = true){
 	let date = new Date(str),
 		time = ("0"+date.getHours()).slice(-2)+":"+("0"+date.getMinutes()).slice(-2);
@@ -14,7 +23,7 @@ const readableTime = function(str = Date.now(), seconds = true){
 				
 	return time;
 };
-			
+
 //initialize chart
 const ctx = document.querySelector("#apichart canvas").getContext("2d");
 const apichart = new Chart(ctx, {
@@ -70,25 +79,26 @@ const apichart = new Chart(ctx, {
 				max:10,
 				display:false
 			},
-			/*x:{
-							type:'time',
-							grid:{
-								
-							}
-						}*/
+			x:{
+				type:"time",
+				time:{
+					ticks:{
+						source:"labels"
+					}
+				},
+				grid:{
+					display:true,
+					color: (line) => (line.index < localStorage.length ? 'rgba(0,0,0,1)' : 'rgba(128,128,128,.5)')
+				}
+			}
 		}
 	}
 });
 			
 const dataRefresh = function(fullUpdate = true){
-	let apikey = "48ce79e682e5e8f79e39cc1374871d75",
-		url_current = "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=36.16754647878633&lon=-86.21153419024921&appid="+apikey,
-		url_forecast = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=36.16754647878633&lon=-86.21153419024921&appid="+apikey,
-		//url_map = "https://tile.openweathermap.org/map/precipitation_new/7/36.1467/-86.8250.png?appid="+apikey,
-		url = (fullUpdate) ? url_forecast : url_current,
+	let url = (fullUpdate) ? url_forecast : url_current,
 		apidata = document.querySelector("#apidata");
 				
-	//fetch current and forecast
 	fetch(new Request(url))
 		.then(response => response.json())
 		.then(data => {
@@ -103,35 +113,37 @@ const dataRefresh = function(fullUpdate = true){
 				apidata.querySelector(".sun .set").innerText = readableTime(data.current.sunset * 1000);
 				apidata.querySelector(".uvi").innerText = data.current.uvi;
 				
-				lastFullUpdate = readableTime();
-				//document.querySelector('#tdata').innerHTML = 'Last Partial: <b>'+lastPartialUpdate+'</b><br>Last Full: <b>'+lastFullUpdate+'</b>';
-							
-				let mainData = {"temp":data.current.temp, "humidity":data.current.humidity, "pressure":data.current.pressure};
-				localStorage.setItem(Date.now(), JSON.stringify(mainData));
-							
 				apichart.data.labels = [];
 				apichart.data.datasets[0].data = [];
 				apichart.data.datasets[1].data = [];
 				apichart.data.datasets[2].data = [];
 				apichart.data.datasets[3].data = [];
-							
-				let i = 0;
+				
+				let logdata = {"temp":data.current.temp, "humidity":data.current.humidity, "pressure":data.current.pressure},
+					apilog = new Array();
+				
+				localStorage.setItem(Date.now(), JSON.stringify(logdata));
+
 				Object.entries(localStorage).forEach(([key,val]) => {
-					let data = JSON.parse(val),
-						dataDate = new Date(key),
-						cutoffDate = new Date(new Date().getTime() - (48 * 60 * 60 * 1000));
+					let dataDate = new Date(parseInt(key)),
+						cutoffDate = new Date(Date.now() - (48 * 60 * 60 * 1000));
 					
-					if(dataDate < cutoffDate) localStorage.removeItem(key);
-					else{
-						apichart.data.labels.push(readableTime((key * 1000), false));
-						apichart.data.datasets[0].data.push(data.pressure);
-						apichart.data.datasets[1].data.push(data.humidity);
-						apichart.data.datasets[2].data.push(data.temp);
-						apichart.data.datasets[3].data.push(0);
-					}
-					i++;
+					if(dataDate < cutoffDate) console.log('data lt cutoff'); //localStorage.removeItem(key);
+					else apilog.push([key,val]);
 				});
-							
+				
+				apilog.sort((a,b) => {return parseInt(a) - parseInt(b);});
+
+				Object.entries(apilog).forEach(lmnt => {
+					let data = JSON.parse(lmnt[1][1]);
+					
+					apichart.data.labels.push(parseInt(lmnt[1][0]));
+					apichart.data.datasets[0].data.push(data.pressure);
+					apichart.data.datasets[1].data.push(data.humidity);
+					apichart.data.datasets[2].data.push(data.temp);
+					apichart.data.datasets[3].data.push(0);
+				});
+
 				data.hourly.forEach(hour => {
 					apichart.data.labels.push(readableTime((hour.dt * 1000), false));
 					apichart.data.datasets[0].data.push(hour.pressure);
@@ -140,34 +152,34 @@ const dataRefresh = function(fullUpdate = true){
 					apichart.data.datasets[3].data.push(hour.pop * 100);
 				});
 							
-				//apichart.options.scales.x.grid = {display:true, color: (line) => (line.index === i ? 'red' : 'rgba(0, 0, 0, 0)')};
 				apichart.update();
+				//localStorage.lastFullUpdate = readableTime();
 			}else{
 				apidata.querySelector(".temp .current").innerText = data.main.temp;
 				apidata.querySelector(".humidity").innerText = data.main.humidity;
 				apidata.querySelector(".pressure").innerText = data.main.pressure;
 				
-				lastPartialUpdate = readableTime();
-				//document.querySelector('#tdata').innerHTML = 'Last Partial: <b>'+lastPartialUpdate+'</b><br>Last Full: <b>'+lastFullUpdate+'</b>';
+				//localStorage.lastPartialUpdate = readableTime();
 			}
-		}).catch(error => {document.querySelector("#nfo").innerHTML += "<br>"+error;});
+			//document.querySelector('#log').innerHTML = 'Last Partial: <b>'+localStorage.lastPartialUpdate+'</b><br>Last Full: <b>'+localStorage.lastFullUpdate+'</b>';
+		}).catch(error => {document.querySelector("#log").innerHTML += "<br>"+error; console.error(error);});
 				
 	/*fetch(url_map)
-					.then(response => response.blob())
-					.then(imageBlob => {
-						// Then create a local URL for that image and print it 
-						const imageObjectURL = URL.createObjectURL(imageBlob);
-						console.log(imageObjectURL);
-					});*/
+		.then(response => response.blob())
+		.then(imageBlob => {
+			// Then create a local URL for that image and print it 
+			const imageObjectURL = URL.createObjectURL(imageBlob);
+			console.log(imageObjectURL);
+		});*/
 };
 dataRefresh();
 			
 setInterval(function run(){
 	let fullUpdate = false;
 					
-	if(++i >= 12){
+	if(++fullupdateFrequency >= 12){
 		fullUpdate = true;
-		i = 0;
+		fullupdateFrequency = 0;
 	}
 				
 	dataRefresh(fullUpdate);
